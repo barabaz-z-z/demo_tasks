92caf77d9b26e39623fec2ab27102317dbc5571e
barabas <vitalii.shulenin@gmail.com>
2020-03-04 11:44:01 +0100

    added create, delete endpoints

--- api/Tasks.API/Controllers/TasksController.cs
+++ api/Tasks.API/Controllers/TasksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -22,25 +23,52 @@ namespace Tasks.API.Controllers
         public TasksController(ILogger<TasksController> logger)
         {
             _logger = logger;
-            _client = new HttpClient();
+            _client = new HttpClient(); // needs using `using` or Dispose
         }
 
         [HttpGet]
         public async Task<IEnumerable<CTask>> GetList()
         {
             var result = await _client.GetStringAsync(_url);
-            var tasks = JsonConvert.DeserializeObject<List<CTask>>(result);
+            var tasks = Deserialize<List<CTask>>(result);
 
             return tasks;
         }
 
         [HttpGet("{id}")]
-        public async Task<CTask> Get([FromRoute] int id)
+        public async Task<CTask> GetById([FromRoute] int id)
         {
-            var result = await _client.GetStringAsync($"{_url}/{id}");
-            var task = JsonConvert.DeserializeObject<CTask>(result);
+            var result = await _client.GetStringAsync($"{_url}/get/{id}");
+            var task = Deserialize<CTask>(result);
 
             return task;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CTask>> Create([FromBody] CTask newTask)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(newTask), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync($"{_url}/create", content);
+            var result = await response.Content.ReadAsStringAsync();
+            var id = Deserialize<int>(result);
+
+            return CreatedAtAction(nameof(GetById), new { Id = id }, newTask);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] int id)
+        {
+            await _client.DeleteAsync($"{_url}/delete/{id}");
+
+            return NoContent();
+        }
+
+        private static TResult Deserialize<TResult>(string json) where TResult : new()
+        {
+            var definition = new { Data = new TResult() };
+            var response = JsonConvert.DeserializeAnonymousType(json, definition);
+
+            return response.Data;
         }
     }
 }
